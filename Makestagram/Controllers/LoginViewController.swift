5cb90907a85c5cf1efc14a30ebedda708a879948
CallmeH <helen.pei.qin@outlook.com>
2018-07-11 15:36:13 -0700

    Initial Setup
    Firebase;
    created folders for tidiness

---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Pei Qin on 2018/7/11.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
